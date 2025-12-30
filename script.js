c93be13df793bb6ffad56ecc34b4c5b5d94c1f18
Prototypestore <dikeledireeks@outlook.com>
2025-12-30 15:12:41 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+fetch('blog.json')
+    .then(res => res.json())
+    .then(posts => {
+        const container = document.getElementById('blog-container');
+        posts.forEach(post => {
+            const article = document.createElement('article');
+            article.className = "blog-card";
+            article.innerHTML = `
+                <img src="${post.image}" alt="${post.title}">
+                <div class="blog-card-content">
+                    <h2>${post.title}</h2>
+                    <p>${post.description}</p>
+                    <button>Read more</button>
+                </div>
+            `;
+            container.appendChild(article);
+        });
+    })
+    .catch(err => console.error("Error loading posts:", err));
